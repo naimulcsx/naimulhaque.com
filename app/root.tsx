b320197f06d9471f9e62a80e56af34e22d74c5f4
Naimul Haque <naimulcsx@gmail.com>
2023-11-23 21:15:24 +0600

    fix: add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -35,6 +35,7 @@ export default function App() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;700&family=Rubik:wght@400;500;600&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <Meta />
         <Links />
         <ColorSchemeScript forceColorScheme="dark" />
